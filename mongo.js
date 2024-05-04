c4b5114152a0025b42b113f13252ac94cd740ecc
Sergio Arias <s-andres140@hotmail.com>
2024-05-04 10:26:52 -0500

    agregue una base de datos pero aun no esta conectada a las notas

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://first_mongo_database:${password}@cluster0.hbnnnz3.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+  content: 'Saludos',
+  important: true,
+})
+
+// Guarda notas en la base de datos
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+// Encuentra todas las notas de la base de datos y las muestra en consola
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
