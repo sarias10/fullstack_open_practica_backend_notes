27d542357365e4fa96cee51c1d2fb79b33d15165
Sergio Arias <s-andres140@hotmail.com>
2024-05-20 16:28:02 -0500

    actualice cosas

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Las variables de entorno definidas en el archivo .env se pueden utilizar con esto
-require('dotenv').config()
+
 const express = require('express')
 const app = express()
+require('dotenv').config()
+
 const cors = require('cors')
 
 const Note = require('./models/note')
@@ -14,34 +16,15 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+// Orden de los middleware es importante
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
-app.use(express.static('dist'))
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({
-    error: 'unknown endpoint'
-  })
-}
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+
+
 
   app.get('/', (request, response) =>{
     response.send('<h1>Hello im sergio World!</h1>')
@@ -53,20 +36,29 @@ let notes = [
     })
   })
 
-  app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note=>{
-      response.json(note)
+  app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note=>{
+      if(note){
+        response.json(note)
+      } else {
+        // .end() se emplea para concluir rápidamente la respuesta sin incluir ningún dato.
+        // 404 = no encontrado
+        response.status(404).end()
+      }
     })
+    //Si se rechaza una promesa devuelta por el método findById
+    .catch(error=> next(error)) // pasa el error a express(), el cual tiene su propio controlador de errores
   })
 
   app.delete('/api/notes/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    notes = notes.filter(note=>note.id!==id)
-
-    response.status(204).end()
+    Note.findByIdAndDelete(request.params.id)
+    .then(result=>{ // el parametro "result" podría usarse para verificar si un recurso realmente se eliminó y podriamos usar esta informacion para devolver códigos de estado diferentes
+      // 204 = sin contenido
+      response.status(204).end()
+    })
+    .catch(error=>next(error))
   })
-
-  
 
   app.post('/api/notes', (request, response) =>{
     const body = request.body
@@ -87,10 +79,49 @@ let notes = [
     })
   })
 
+  app.put('/api/notes/:id', (request, response, next)=>{
+    const body = request.body
+
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+    
+  })
+
+
+
+
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({
+      error: 'unknown endpoint'
+    })
+  }
+  
+  // handler of requests with unknown endpoint
+  app.use(unknownEndpoint)
   
 
-  app.use(unknownEndpoint)
- 
+
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    console.log(error.name)
+    if(error.name==='CastError') {
+      return response.status(400).send({error: 'malformatted id'})
+  
+    }
+    next(error)
+  }
+
+  // handler of requests with result to errors
+  app.use(errorHandler)
   
 
 
