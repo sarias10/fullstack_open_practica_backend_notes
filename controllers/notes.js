b7191f0f16a811c17b04e7bfa707390e94ad3e4d
Sergio Arias <s-andres140@hotmail.com>
2024-06-11 11:38:49 -0500

    parte 4-8

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,12 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 
 notesRouter.get('/', async (request, response) => {
-    const notes = await Note.find({})
+    const notes = await Note
+        .find({}).populate('user', { username: 1, name: 1 })
+
     response.json(notes)
 })
 
@@ -22,12 +25,18 @@ notesRouter.get('/:id', async (request, response) => {
 notesRouter.post('/', async (request, response) => {
     const body = request.body
 
+    const user = await User.findById(body.userId)
+
     const note = new Note({
         content: body.content,
-        important: body.important || false,
+        important: body.important === undefined ? false: body.important,
+        user: user.id
     })
 
     const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
     //un código 201 significa que una solicitud se procesó correctamente y devolvió,o creó, un recurso o resources en el proceso
     response.status(201).json(savedNote)
 })
