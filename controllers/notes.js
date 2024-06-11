d62d56ee5ac164034dcb474871b37fe17f62a17d
Sergio Arias <s-andres140@hotmail.com>
2024-06-11 09:44:31 -0500

    actualizacion parte 4-7

--- controllers/notes.js
+++ controllers/notes.js
@@ -7,7 +7,7 @@ notesRouter.get('/', async (request, response) => {
     response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
+notesRouter.get('/:id', async (request, response) => {
     const note = await Note.findById(request.params.id)
     if(note){
         response.json(note)
