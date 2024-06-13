faea6f0b1580e7f201684ac7e2e9a7b851c312d1
Sergio Arias <s-andres140@hotmail.com>
2024-06-13 18:56:27 -0500

    parte 4-9

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -30,9 +31,9 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/users',usersRouter)
-app.use('/api/notes',notesRouter)
-
+app.use('/api/users', usersRouter)
+app.use('/api/notes', notesRouter)
+app.use('/api/login', loginRouter)
 
 // handler of requests with unknown endpoint
 app.use(middleware.unknownEndpoint)
